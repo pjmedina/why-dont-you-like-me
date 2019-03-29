16c8c97af1241bbe30e736a2d3a3c5db52820e42
Pia Medina <pjmedina@edu.uwaterloo.ca>
2019-03-29 14:07:35 -0400

    include dictionary to store info

--- app-button.py
+++ app-button.py
@@ -1,28 +1,13 @@
 
-"""
-Demo Flask application to test the operation of Flask with socket.io
-Aim is to create a webpage that is constantly updated with random numbers from a background python process.
-30th May 2014
-===================
-Updated 13th April 2018
-+ Upgraded code to Python 3
-+ Used Python3 SocketIO implementation
-+ Updated CDN Javascript and CSS sources
-"""
-
-
-
-
 # Start with a basic flask app webpage.
 from flask import Flask, render_template, url_for, copy_current_request_context
 from flask_socketio import SocketIO, emit
-from random import random
 from time import sleep
+import datetime
+import pandas as pd
 from threading import Thread, Event
 
-#impor
-
-
+# author of the skeleton code
 __author__ = 'slynn'
 
 app = Flask(__name__)
@@ -38,11 +23,11 @@ from time import sleep     # this lets us have a time delay (see line 15)
 input_port = 12
 output_port = 13
 GPIO.setmode(GPIO.BCM)     # set up BCM GPIO numbering
-GPIO.setup(input_port, GPIO.IN, pull_up_down=GPIO.PUD_OFF)    # set GPIO25 as input (button)
-GPIO.setup(output_port, GPIO.OUT)   # set GPIO24 as an output (LED)
+GPIO.setup(input_port, GPIO.IN, pull_up_down=GPIO.PUD_OFF)    # set GPIO12 as input (button)
+GPIO.setup(output_port, GPIO.OUT)   # set GPIO13 as an output (LED)
 
 
-#random number Generator Thread
+#create thread
 thread = Thread()
 thread_stop_event = Event()
 
@@ -51,41 +36,41 @@ class RandomThread(Thread):
         self.delay = 0.01
         super(RandomThread, self).__init__()
 
-    def randomNumberGenerator(self):
+    def countButtonPush(self):
         lastButtonState = 0
-        number = 0
-        """
-        Generate a random number every 1 second and emit to a socketio instance (broadcast)
-        Ideally to be run in a separate thread?
-        """
-        #infinite loop of magical random numbers
-        # print("Making random numbers")
-        # while not thread_stop_event.isSet():
-        #     number = round(random()*10, 3)
-        #     print(number)
-        #     socketio.emit('newnumber', {'number': number}, namespace='/test')
-        #     sleep(self.delay)
-
+        number = -1
+        dict = {}
         try:
-            while not thread_stop_event.isSet():         # this will carry on until you hit CTRL+C
-                if GPIO.input(input_port) != lastButtonState: # if port 25 == 1
+            # this will carry on until you hit CTRL+C
+            while not thread_stop_event.isSet():
+                #register button push
+                if GPIO.input(input_port) != lastButtonState:
                     lastButtonState = GPIO.input(input_port)
-                    print( "button Tranition Detected")
+                    print( "Button Tranition Detected")
                     if lastButtonState == 1:
+                        #increase counter by 1
                         number = number +1
+                        # sending number to the client
                         socketio.emit('newnumber', {'number': number}, namespace='/test')
                         print(number)
-                        GPIO.output(output_port, 1)         # set port/pin value to 1/HIGH/True
+                        GPIO.output(output_port, 1)
+
+                        #update dict with new count and time
+                        time = datetime.datetime.now()
+                        dict[time] = number
+                        count_df = pd.DataFrame.from_dict(dict, orient='index')
+                        count_df.to_csv('data/box1_data.csv')
+                        # print(count_df)
+
                 else:
                     print("Port 25 is 0/LOW/False - LED OFF")
                     GPIO.output(output_port, 0)         # set port/pin value to 0/LOW/False
                 sleep(self.delay)        # wait 0.1 seconds
-
         finally:                   # this block will run no matter how the try block exits
             GPIO.cleanup()         # clean up after yourself
 
     def run(self):
-        self.randomNumberGenerator()
+        self.countButtonPush()
 
 
 @app.route('/')
