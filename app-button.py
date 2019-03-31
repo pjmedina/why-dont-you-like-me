93999313421255dff923945e71eee4ead8851906
Pia Medina <pjmedina@edu.uwaterloo.ca>
2019-03-31 00:08:14 -0400

    fsdf

--- app-button.py
+++ app-button.py
@@ -39,7 +39,8 @@ try:
     i2c = busio.I2C(board.SCL, board.SDA)
     drv = adafruit_drv2605.DRV2605(i2c)
     drv.sequence[0] = adafruit_drv2605.Effect(47)
-except OSError as e:
+except ValueError as e:
+    print(e)
     haptics = False
 
 
