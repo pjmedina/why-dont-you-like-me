c97ad7faae7eff635186c15955a0ca194e50b73f
Pia Medina <pjmedina@edu.uwaterloo.ca>
2019-03-30 22:28:58 -0400

    deleting comment

--- static/js/application.js
+++ static/js/application.js
@@ -2,15 +2,6 @@ $(document).ready(function(){
     //connect to the socket server.
     var socket = io.connect('http://' + document.domain + ':' + location.port + '/test');
 
-    // //receive details from server
-    // socket.on('newnumber', function(msg) {
-    //     console.log("Received number" + msg.number);
-    //     console.warn("Received number" + msg.number);
-    //     number_string = '<h1>' + msg.number.toString() + '</h1>';
-    //     $('#log').html(number_string);
-    //
-    // });
-
     socket.on('newnumber',function(msg) {
         const numberElement = document.getElementById('log');
         numberElement.innerHTML = '';
