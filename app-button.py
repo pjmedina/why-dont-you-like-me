eac308a615cb4ac7a9530972b13162b8262a03e5
Pia Medina <pjmedina@edu.uwaterloo.ca>
2019-03-31 00:04:00 -0400

    catch OSError

--- app-button.py
+++ app-button.py
@@ -39,8 +39,9 @@ try:
     i2c = busio.I2C(board.SCL, board.SDA)
     drv = adafruit_drv2605.DRV2605(i2c)
     drv.sequence[0] = adafruit_drv2605.Effect(47)
-except:
+except OSError as e:
     haptics = False
+
 
 #create thread
 thread = Thread()
