0bf312c5c425ef364e4b2dbdec8cdff8dc6e031b
Alex Bondarenko <abondare@edu.uwaterloo.ca>
2019-03-31 14:02:00 -0400

    added kiosk script

---
+++ kiosk.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+pkill flask
+pkill python
+pkill chromium
+xset s noblank
+xset s off
+xset -dpms
+
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
+sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
+
+/usr/bin/chromium-browser --noerrdialogs --disable-infobars --incognito  --kiosk http://127.0.0.1:5000 &
+
+runFlaskStuff () {
+cd ~/Documents/why-dont-you-like-me/
+pwd
+export "FLASK_APP=app-button.py"
+flask run
+}
+updateCode () {
+cd ~/Documents/why-dont-you-like-me/
+git pull
+}
+
+updateCode
+runFlaskStuff
+
