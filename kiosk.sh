ff453db3468a97604781be48707614da98cb8002
Alex Bondarenko <abondare@edu.uwaterloo.ca>
2019-04-01 14:02:17 -0400

    trying faster boot

--- kiosk.sh
+++ kiosk.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-pkill flask
-pkill python
-pkill chromium
+
+launchChrome () {
 xset s noblank
 xset s off
 xset -dpms
@@ -10,6 +9,7 @@ sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromi
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
 
 /usr/bin/chromium-browser --noerrdialogs --disable-infobars --incognito  --kiosk http://127.0.0.1:5000 &
+}
 
 runFlaskStuff () {
 cd ~/Documents/why-dont-you-like-me/
@@ -17,11 +17,13 @@ pwd
 export "FLASK_APP=app-button.py"
 flask run
 }
+
 updateCode () {
 cd ~/Documents/why-dont-you-like-me/
 git pull
 }
 
 #updateCode
-runFlaskStuff
 
+runFlaskStuff &
+launchChrome
